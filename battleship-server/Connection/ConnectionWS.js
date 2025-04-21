4ad31a57bc20e02c11b823f350d5d8107a624467
OgneFF <wyrik9324@gmail.com>
2025-04-21 15:37:47 +0300

    Исправил баг

--- battleship-server/Connection/ConnectionWS.js
+++ battleship-server/Connection/ConnectionWS.js
@@ -1,8 +1,9 @@
 
 const WebSocket = require('ws');
 const { handleConnection } = require('./gameManager');
+const port = process.env.PORT || 8080;
 
-const webSocketConnect = new WebSocket.Server({ port: 8080 });
+const webSocketConnect = new WebSocket.Server({ port });
 
 webSocketConnect.on('connection', (ws) => {
   handleConnection(ws);
